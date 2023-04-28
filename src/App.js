8de67299719847e5b61abc77e1a86a1d4f242ab5
NUTThanadol <265345221011-st@rmutsb.ac.th>
2023-04-28 11:31:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import {Navbar,Header,Footer} from './func-components'
 import MsgBox from './func-props'
 import {MyAbout,MyImage,MyData} from './func-mydata'
 import Caledar from './class-component'
-
+import MsgBox2 from './class-props'
+import List from './list comp'
 
 function App() {
-  return [<Navbar />,<Header />,<Caledar/>,<Footer />]
- /* return(
-    <MsgBox
+
+  return (<List/>)
+
+
+  //return [<Navbar />,<Header />,<Caledar/>,<Footer />,<Msgbox2/>]
+  /*return(
+    <MsgBox2
       text="Using props in function coppoment"
       color="red"
       bgcolor="#ccc"
@@ -16,9 +21,10 @@ function App() {
       border="dotted 1px black" 
     />
 
-  )*/
-  //return [<MyAbout/>,<MyImage/>,<Caledar/>,<MyData/>]
+  )
+  //return [<MyAbout/>,<MyImage/>,<Caledar/>,<MyData/>]*/
 }
+
 
 
 export default App;
